5f5ed58f940d392d767762b6fedb39c23611653d
bilelZahi <bilelzahi91@gmail.com>
2021-03-31 13:03:21 +0100

    helloo

--- Backend/routes/userApi.js
+++ Backend/routes/userApi.js
@@ -2,21 +2,32 @@ const express = require('express');
 
 const user = require('../models/userSchema');
 const router = express.Router();
+// const jwt = require("jsonwebtoken");
+
 
 
 // register
-router.post('/addNewUser',(req,res) => {
-  console.log(req.body);
-      user.create(req.body)
-       .then(result =>{res.json(result)})
-       .catch(error =>console.log(error));
-     
-     
-     });
+router.post('/register', async (req, res) => {
+    
+    const userFound = await user.findOne({email: req.body.email});
+    if(!userFound)
+    {
+     return res.json({message: 'E-mail address is already used!'});
+    }
+  
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    
+    const userData = req.body; 
+    userData.password = hash; 
+ 
+ const createdUser = await user.create(userData);
+ res.json(createdUser);
+});
     //  login
     router.post('/login', async (req, res) => {
-      const connectedUser = await User.findOne({email: req.body.email , password : req.body.password});
-      if(connectedUser ===null)
+      const connectedUser = await user.findOne({email: req.body.email , password : req.body.password});
+      if(!connectedUser )
       {
          return res.json({message: 'email or password is invalid!'});
       }   
@@ -24,5 +35,5 @@ router.post('/addNewUser',(req,res) => {
                 return res.json({message: 'login successfully!'});
           }
       }
-  });
+  );
 module.exports = router;
