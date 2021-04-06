deab120eb3f16e8d2f310c9540a04195ce5cb79e
bileltrabelsi20 <bileltrabelsi445@gmail.com>
2021-04-06 14:46:35 +0100

    burger_sandwich_tacos

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,8 @@ const connect = require('./dataBase/connect');
 const userApi = require('./routes/userApi');
 const sandwichSchemaApi = require ('./routes/sandwichSchemaApi');
 const ingrediantsSchemaApi = require ('./routes/ingrediantsSchemaApi')
+const burgerSchemaApi = require ('./routes/burgerSchemaApi')
+const tacosSchemaApi = require ('./routes/tacosSchemaApi')
 
 const bodyParser = require('body-parser');
 
@@ -12,13 +14,13 @@ const app = express();
 app.use(bodyParser.json());
 var urlencodeParser = bodyParser.urlencoded({extended:false});
 
-// const path = require('path');
-// const cors = require('cors');
+app.use ('/user', userApi);
+app.use ('/login',userApi);
+app.use ('/menu' , sandwichSchemaApi);
+app.use ('/menu' , ingrediantsSchemaApi);
+app.use ('/menu' , burgerSchemaApi);
+app.use ('/menu' , tacosSchemaApi);
 
-app.use('/user', userApi);
-app.use('/login',userApi);
-app.use ('/menu' , sandwichSchemaApi)
-app.use ('/menu' , ingrediantsSchemaApi)
 
 
 
--- Backend/models/burgerSchema.js
+++ Backend/models/burgerSchema.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const sandwichSchema = new Schema({
-    prixPrincipale : String ,
-    nom : String ,
+const burgerSchema = new Schema({
+    
+    prixBurger : String ,
+    nomBurger : String ,
     compositions : Array,
     image : String,
 
     ingrediants : [{type : Schema.Types.ObjectId , ref : 'ingrediant'}],
 
+
     
 }, {timesTamps:true});
 
-const sandwich = mongoose.model('sandwich',sandwichSchema);
-module.exports = sandwich
+const burger = mongoose.model('burger',burgerSchema);
+module.exports = burger
