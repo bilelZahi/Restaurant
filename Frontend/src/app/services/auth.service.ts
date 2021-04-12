c1cff94a18453dd9f8d111144746521cfeaafe64
bileltrabelsi20 <bileltrabelsi445@gmail.com>
2021-04-12 16:11:43 +0100

    fix register

--- Frontend/src/app/services/auth.service.ts
+++ Frontend/src/app/services/auth.service.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { BehaviorSubject, Observable } from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  isLoginSubject = new BehaviorSubject<boolean>(false);
 
   baseURL = environment.baseURL
 
@@ -19,23 +17,17 @@ export class AuthService {
 
     return this.http.post( this.baseURL + '/user/register',data)
   }
-  mail(body){
+  mail(body) {
     
     return this.http.post( this.baseURL + '/mail/sendMail',body)
 
   }
-  login(data)
-  {
+
+  login(data){
+
      return this.http.post(this.baseURL + '/user/login', data)
+
   }
-  public getToken(): string {
-    return JSON.parse(localStorage.getItem('token'))
-  }
-  signInSubject(){
-    localStorage.setItem('token', 'JWT');
-    this.isLoginSubject.next(true); 
-    
-   }
  
 }
 
