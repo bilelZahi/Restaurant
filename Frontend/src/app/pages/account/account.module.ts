8127c594f0e24f71be245e14ce9e4d1daec1fe31
bilelZahi <bilelzahi91@gmail.com>
2021-04-15 11:25:44 +0100

    fix products

--- Frontend/src/app/pages/account/account.module.ts
+++ Frontend/src/app/pages/account/account.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../../shared/shared.module';
 import { AccountComponent } from './account.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { InformationComponent } from './information/information.component';
 import { AddressesComponent } from './addresses/addresses.component';
 import { OrdersComponent } from './orders/orders.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { MatBadgeModule, MatDialogModule } from '@angular/material';
+import { ProductsComponent } from './products/products.component';
 
 export const routes = [
   { 
@@ -17,7 +20,10 @@ export const routes = [
           { path: 'dashboard', component: DashboardComponent, data: {  breadcrumb: 'Dashboard' } },
           { path: 'information', component: InformationComponent, data: {  breadcrumb: 'Information' } },
           { path: 'addresses', component: AddressesComponent, data: {  breadcrumb: 'Addresses' } },
-          { path: 'orders', component: OrdersComponent, data: {  breadcrumb: 'Orders' } }
+          { path: 'orders', component: OrdersComponent, data: {  breadcrumb: 'Orders' } },
+          {path: 'addProduct' , component:AddProductComponent},
+          {path:'products' , component:ProductsComponent}
+
       ]
   }
 ];
@@ -27,14 +33,20 @@ export const routes = [
     CommonModule,
     RouterModule.forChild(routes),
     ReactiveFormsModule,
-    SharedModule
+    FormsModule,
+    SharedModule,
+    MatBadgeModule,
+    MatDialogModule
+   
   ],
   declarations: [
     AccountComponent,
     DashboardComponent,
     InformationComponent,
     AddressesComponent,
-    OrdersComponent
+    OrdersComponent,
+    AddProductComponent,
+    ProductsComponent
   ]
 })
 export class AccountModule { }
